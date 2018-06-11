78e449b512a3bbead367201fbf79ca4d6f44385c
marcelojw8 <marcelojw8@gmail.com>
2018-06-11 20:00:44 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/android/embarcadost2/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.example.wille.embarcados;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class BubbleSort {
+
+    public ArrayList<Integer> selectionSort(ArrayList<Integer> arr) {
+
+        ArrayList<Integer> array = arr;
+        int temp;
+
+        for (int i = 0; i < array.size() - 1; i++) {
+            for (int j = 0; j < array.size() - 1; j++) {
+                if (array.get(i) > array.get(j+1)) {
+                    temp = array.get(j);
+                    array.set(j, array.get(j+1));
+                    array.set(j+1, temp);
+                }
+            }
+        }
+
+        return array;
+    }
+}
